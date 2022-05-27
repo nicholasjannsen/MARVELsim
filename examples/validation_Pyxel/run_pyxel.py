21f9ead552baadd88e169eb1e4878dc7657f0929
Nicholas Jannsen <nicholasemborgjannsen@gmail.com>
2022-05-27 18:37:57 +0200

    Updates of validation notebook and inputfiles

--- examples/validation_Pyxel/run_pyxel.py
+++ examples/validation_Pyxel/run_pyxel.py
@@ -5,22 +5,21 @@ import numpy as np
 
 # Load specific inputfile for MARVEL
 config = pyxel.load("../../inputfiles/inputfile_marvel.yaml")
+#config = pyxel.load("inputfile.yaml")
 
 # Setup configurations
 exposure = config.exposure        # class Observation
 detector = config.ccd_detector    # class CCD
 pipeline = config.pipeline        # class DetectionPipeline
 
+# Exposure class
+pipeline.photon_generation.cosmix.arguments.spectrum_file          = "../../inputfiles/proton_L2_solarMax_11mm_Shielding.txt"
+pipeline.photon_generation.cosmix.arguments.seed                   = np.random.randint(1e9)
+pipeline.photon_generation.cosmix.arguments.particles_per_second   = 0
+pipeline.charge_generation.load_charge.arguments.filename          = 'bias_slow.fits'
+pipeline.charge_generation.simple_dark_current.arguments.dark_rate = 0.01
+pipeline.charge_generation.load_charge.arguments.time_scale        = 1
+exposure.readout.times  = [1]
+
 # Run pyxel in exposure mode
-pipeline.photon_generation.cosmix.arguments.spectrum_file        = "../../inputfiles/proton_L2_solarMax_11mm_Shielding.txt"
-pipeline.photon_generation.cosmix.arguments.seed                 = np.random.randint(1e9)
-pipeline.photon_generation.cosmix.arguments.particles_per_second = 0
-pipeline.charge_generation.load_charge.arguments.filename        = 'science.fits'
-pipeline.charge_generation.load_charge.arguments.time_scale      = 1
-pipeline.charge_generation.dark_current.arguments.dark_rate      = 0.1
-
-#exposure = '[1]'
-#print(exposure)
-#exit()
-
 results = pyxel.exposure_mode(exposure=exposure, detector=detector, pipeline=pipeline)
