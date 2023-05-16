75ed89839120f6ea3a9ef99f298acf5704b7e73a
Nicholas Jannsen <nicholasemborgjannsen@gmail.com>
2023-05-16 11:53:11 +0200

    New validation script background.py

--- validation/background.py
+++ validation/background.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 
 """
-This simulation tool is designed specifically for the upcoming MARVEL
-spectroscopic survey consisting of 4 small robotic telescopes each of
-a 0.8 primary mirror diameter, and combined by fibers to a highly stable
-high resolution spectrographs. This simulator uses the PyEchelle code
-to simulate realistic MARVEL spectra using a ray tracing technique, and
-the Pyxel code that adds various important CCD effects. Seen above this
-software can be used to simulate standard calibrated spectra and stellar
-spectra including a RV signal. Used in combination with High Performance
-Computing (HPC) this makes it easy to simulate a time series of spectra.
+Small script to generate the background.
 """
 
 # Defaults
@@ -21,10 +13,9 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.visualization import SqrtStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
-from astropy.stats import biweight_location, mad_std
-from astropy.stats import SigmaClip
+from astropy.stats import SigmaClip, sigma_clipped_stats
 from photutils.background import Background2D, MedianBackground
-from scipy import interpolate
+from scipy.interpolate import RectBivariateSpline
 
 #==============================================================#
 #                           UTILITIES                          #
@@ -36,40 +27,42 @@ hdr  = hdul[0].header
 data = hdul[0].data
 
 # Reduce some statistics
-#print(f'Median   value: {np.median(data)}')
-#print(f'Biweight value: {biweight_location(data)}')
-#print(f'MAD std  value: {mad_std(data)}')
+mean, median, std = sigma_clipped_stats(data, sigma=3.0)
+print('Statistics BEFORE background subtracted')
+print(f'mean   value: {mean}')
+print(f'median value: {median}')
+print(f'std    value: {std}')
 
-# Perform 
+# Determine the 2D background image
 sigma_clip = SigmaClip(sigma=3.0)
 bkg_estimator = MedianBackground()
 bkg = Background2D(data, (50, 50), filter_size=(3, 3),
                    sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
 
 # Statistics for background image
+print('Statistics AFTER background subtracted')
 print(f'Bg median: {bkg.background_median}')
 print(f'Bg rms   : {bkg.background_rms_median}')
 
 # New image
 bg    = bkg.background
 data1 = data - bg
+norm = ImageNormalize(stretch=SqrtStretch())
 
 # Plot data
-norm = ImageNormalize(stretch=SqrtStretch())
 fig, ax = plt.subplots(1, 3, figsize=(15,10))
 ax[0].imshow(data,  norm=norm, origin='lower', cmap='Greys_r', interpolation='nearest')
 ax[1].imshow(bg,    norm=norm, origin='lower', cmap='Greys_r', interpolation='nearest')
 ax[2].imshow(data1, norm=norm, origin='lower', cmap='Greys_r', interpolation='nearest')
 plt.show()
 
-
-# Interpolate image
+# Interpolate image to MARVEL size
 x = np.linspace(0, 1, bg.shape[0])
 y = np.linspace(0, 1, bg.shape[1])
-f = interpolate.interp2d(y, x, bg, kind='cubic')
+f = RectBivariateSpline(x, y, bg) #, kind='cubic')
 x1 = np.linspace(0, 1, 10560)
 y1 = np.linspace(0, 1, 10560)
-bg1 = f(y1, x1)
+bg1 = f(x1, y1)
 
 # Plot data
 plt.figure()
